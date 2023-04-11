d6f655b34e6fe453e7db2e73a9df54c33544eaa8
Pawday <pawday@mail.ru>
2023-04-11 17:27:21 +0300

    [cmake]: Implement advanced cmake hack for disabling "/showIncludes" option in MSVC+Ninja toolchain

---
+++ cmake/CMakeBuildSystemHacks.cmake
@@ -0,0 +1,37 @@
+macro(DisableIncludeTreeDumpOnNinjaWithMSVC)
+    if (${CMAKE_GENERATOR} STREQUAL "Ninja" AND MSVC)
+        option(MSVC_NINJA_DISABLE_INCLUDE_TREE_DUMP "If true option \"/showIncludes\" of msvc compiler would be erased" ON)
+        option(DISABLE_HINT_MSVC_NINJA "Disables hint about Ninja's include tree dumping on MSVC" OFF)
+
+        if (NOT ${MSVC_NINJA_DISABLE_INCLUDE_TREE_DUMP} AND NOT ${DISABLE_HINT_MSVC_NINJA})
+
+            list(APPEND HINT_MESSAGE "MSVC and Ninja toolchain detected")
+            list(APPEND HINT_MESSAGE "option \"MSVC_NINJA_DISABLE_INCLUDE_TREE_DUMP\" was explicitly set to OFF")
+            list(APPEND HINT_MESSAGE "you will probably see the ENORMOUS include tree on compilation for EACH source file")
+            list(APPEND HINT_MESSAGE "Here some links about it:")
+            list(APPEND HINT_MESSAGE "https://discourse.cmake.org/t/showincludes-option-added-automatically/6706")
+            list(APPEND HINT_MESSAGE "https://github.com/ninja-build/ninja/issues/2138")
+            list(APPEND HINT_MESSAGE "https://github.com/ninja-build/ninja/issues/613")
+            list(APPEND HINT_MESSAGE "I'd recommend you enable that option back for convenience")
+
+
+            foreach(line ${HINT_MESSAGE})
+                message(NOTICE "[CMAKE_DEV_HINT] " ${line})
+            endforeach()
+
+            message(NOTICE "[CMAKE_DEV_HINT] ")
+            message(NOTICE "[CMAKE_DEV_HINT] To disable this [CMAKE_DEV_HINT] message set \"DISABLE_HINT_MSVC_NINJA\" option to ON")
+
+            unset(HINT_MESSAGE)
+        endif ()
+    else()
+        return()
+    endif()
+
+    if (${MSVC_NINJA_DISABLE_INCLUDE_TREE_DUMP})
+
+        string(REPLACE "/showIncludes" "" CMAKE_DEPFILE_FLAGS_C ${CMAKE_DEPFILE_FLAGS_C})
+        string(REPLACE "/showIncludes" "" CMAKE_DEPFILE_FLAGS_CXX ${CMAKE_DEPFILE_FLAGS_CXX})
+
+    endif()
+endmacro()
